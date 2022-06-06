26d61b545a4f4b559f932a5578f1caf30841cefe
Pawl D <orc-warrior@cox.net>
2022-06-06 13:06:16 -0500

    Created the relevant User models

---
+++ Source/BusinessEntities/BusinessEntities/User/UserBE.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Web.App.BusinessEntities.User
+{
+    public class UserBE
+    {
+        /// <summary>
+        /// The ID number of the user.
+        /// </summary>
+        public int UserID;
+        /// <summary>
+        /// The unique username of the user.
+        /// </summary>
+        public string UserName;
+        /// <summary>
+        /// The current balance of the user.
+        /// </summary>
+        public int Balance;
+    }
+}
---
+++ Source/Data/Blackjack.Data/Entities/UserEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Web.App.Data.Entities
+{
+    public class UserEntity
+    {
+        /// <summary>
+        /// The ID number of the user.
+        /// </summary>
+        public int UserID;
+        /// <summary>
+        /// The unique username of the user.
+        /// </summary>
+        public string UserName;
+        /// <summary>
+        /// The Soundex representation of the user.
+        /// Used for quick index searches.
+        /// </summary>
+        public string Soundex;
+        /// <summary>
+        /// The current balance of the user.
+        /// </summary>
+        public int Balance;
+
+    }
+}
